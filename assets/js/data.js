47427fbb38e56ca7deca46de4c84a294c46e293a
clvrdesign628 <clvrdesign628@gmail.com>
2026-09-09 13:32:35 +0200

    Update member data structure and enhance member identification section in the engagement charter

--- assets/js/data.js
+++ assets/js/data.js
@@ -3,11 +3,14 @@ const memberData = {
   firstname: 'Ezéchiel',
   lastname: 'SINZINKAYO',
   number: '02534652',
+  phone: '79 123 456',
+  email: 'sinzinkayo@gmail.com',
   signingDate: new Date().toLocaleDateString('fr-FR', {
     day: '2-digit',
     month: '2-digit',
     year: 'numeric',
   }),
+  photoSrc: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   signatureSrc: 'https://static.vecteezy.com/ti/vecteur-libre/p1/59415135-elegant-noir-signature-faux-un-autographe-echantillons-elegant-manuscrit-un-autographe-numerique-encre-signature-isole-sur-transparent-contexte-pour-exemples-de-documents-certificats-et-contrats-vectoriel.jpg',
   qrCodeSrc: 'https://upload.wikimedia.org/wikipedia/commons/d/d0/QR_code_for_mobile_English_Wikipedia.svg?utm_source=en.wikipedia.org&utm_campaign=index&utm_content=original',
 };
