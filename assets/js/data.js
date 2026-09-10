3c55317f68378acda4c49aa8434c0cd33b3a9d77
clvrdesign628 <clvrdesign628@gmail.com>
2026-09-10 12:44:36 +0200

    Refactor code structure for improved readability and maintainability

--- assets/js/data.js
+++ assets/js/data.js
@@ -4,6 +4,8 @@ const memberData = {
   lastname: 'SINZINKAYO',
   number: '34652',
   statut: 'Full Member',
+  substatut: 'Membre à part entière',
+  statutIconSrc: './assets/icons/circle-star.svg',
   phone: '79 123 456',
   email: 'sinzinkayo@gmail.com',
   signingDate: new Date().toLocaleDateString('fr-FR', {
@@ -11,9 +13,9 @@ const memberData = {
     month: '2-digit',
     year: 'numeric',
   }),
-  photoSrc: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-  signatureSrc: 'https://static.vecteezy.com/ti/vecteur-libre/p1/59415135-elegant-noir-signature-faux-un-autographe-echantillons-elegant-manuscrit-un-autographe-numerique-encre-signature-isole-sur-transparent-contexte-pour-exemples-de-documents-certificats-et-contrats-vectoriel.jpg',
-  qrCodeSrc: 'https://upload.wikimedia.org/wikipedia/commons/d/d0/QR_code_for_mobile_English_Wikipedia.svg?utm_source=en.wikipedia.org&utm_campaign=index&utm_content=original',
+  photoSrc: './assets/images/ezechiel.png',
+  signatureSrc: './assets/images/signature.png',
+  qrCodeSrc: './assets/images/qr-code.webp',
 };
 
 function hydrate(data) {
